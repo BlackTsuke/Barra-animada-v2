feaba28189214a5b454fa120227ef1c8058d9149
Mikael Silva Cunha <100884743+BlackTsuke@users.noreply.github.com>
2024-09-29 07:10:59 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const search = document.querySelector('.search')
-const barra = document.querySelector('#barra')
-
-search.addEventListener('click', handleClick)
-
-
-function handleClick() {
-  barra.classList.toggle('active')
-}
